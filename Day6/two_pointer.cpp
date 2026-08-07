56c2b3685d2187d34957de4c07210c1568ead7bf
subodhnarayan <subodhnarayansah@gmail.com>
2026-08-08 00:36:48 +0530

    adding code for count of subarry having sum is less than equal to k in cpp

--- Day6/two_pointer.cpp
+++ Day6/two_pointer.cpp
@@ -1,11 +1,61 @@
-Number of subarrays whose sum <= K
+// Number of subarrays whose sum <= K
+
+#include<bits/stdc++.h>
+using namespace std;
 
 
-int twopointer(vector<int> arr){
+int twopointerbruteforce(vector<int> arr,int k){
+
+    //TC=O(n2) and SC=0(n)
+    int n=arr.size();
+    int sum=0;
+    int count=0;
+    //vector<int> arr={2,1,1,5,8};
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+          sum+=arr[j];
+          if(sum<=k){
+            count++;
+          }
+
+        }
+         
+    }
+    return count;
+}
+
+
+
+
+int twopointer(vector<int> arr,int k){
+
+    //TC=O(2n)=(n) and SC=o(n)
     int n=arr.size();
     int i=0;
     int sum=0;
+    int count=0;
     for(int j=0;j<n;j++){
-
+        sum+=arr[j];
+        while(sum>k){
+            sum-=arr[i];
+            i++;
+        }
+       count+=(j-i+1);
     }
+    return count;
+}
+
+int main(){
+    // int n;
+    // cin>>n;
+    // int k;
+    // cin>>k;
+    vector<int> arr={2,1,1,5,8};
+    // for(int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    cout<<twopointer(arr,4);
+    cout<<"\n";
+    cout<<twopointerbruteforce(arr,4);
 }
