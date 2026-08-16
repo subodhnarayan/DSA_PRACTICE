7b1ad395a19717582caa1c156e13fc355d2f2134
subodhnarayan <subodhnarayansah@gmail.com>
2026-08-16 22:13:34 +0530

    adding the optimized solution for the count of subarray whose sum is less than equal k

--- Day15_Revision/count of subarray whose sum is less than eqal k.py
+++ Day15_Revision/count of subarray whose sum is less than eqal k.py
@@ -2,17 +2,33 @@ def countsubarray(arr,k):
     n=len(arr)
     sum=0
     count=0
+    i=0
 
     #Brute Force Approch
 
     #TC=0(n2) and SC=(n)
 
-    for i in range(0,n):
-        for j in range(i,n):
-            sum+=arr[j]
+    # for i in range(0,n):
+    #     for j in range(i,n):
+    #         sum+=arr[j]
 
-            if sum<=k:
-                count+=1
+    #         if sum<=k:
+    #             count+=1
+
+
+    for j in range(0,n):
+        sum+=arr[j]
+        # Shrink window if sum exceeds k, but don't let i pass j
+        while sum>k and i<=j:
+            sum-=arr[i]
+            i+=1
+
+        # Count all valid subarrays ending at index j
+        count+=(j-i+1)       # This will the overal count of the subarray after adding that number
+
+
+
+    return count
 
 
 
