33e3b6df0110e88f6e001bc5fe8eff0410c1ae3d
subodhnarayan <subodhnarayansah@gmail.com>
2026-08-20 23:59:42 +0530

    binary search

---
+++ Day19_BinarySearch/Binary Search.cpp
@@ -0,0 +1,33 @@
+/*
+// Binary Search
+// Difficulty: BasicAccuracy: 48.03%Submissions: 407K+Points: 1Average Time: 20m
+// Given an array arr[], sorted in ascending order and an integer k. Return true if k is present in the array, otherwise, false.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4, 6], k = 6
+// Output: true
+// Exlpanation: Since, 6 is present in the array at index 4 (0-based indexing), output is true.
+// Input: arr[] = [1, 2, 4, 5, 6], k = 3
+// Output: false
+// Exlpanation: Since, 3 is not present in the array, output is false.
+// Input: arr[] = [2, 3, 5, 6], k = 1
+// Output: false
+// Constraints:
+// 1 ≤ arr.size() ≤ 106
+// 1 ≤ k ≤ 106
+// 1 ≤ arr[i] ≤ 106
+
+// Expected Complexities
+// Time Complexity: O(log n)
+// Auxiliary Space: O(1)
+
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
