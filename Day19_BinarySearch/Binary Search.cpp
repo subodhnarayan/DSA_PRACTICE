888db4ecbbfa990e879901fc709054efe4324f70
subodhnarayan <subodhnarayansah@gmail.com>
2026-08-21 02:38:18 +0530

    adding the code of binary search

--- Day19_BinarySearch/Binary Search.cpp
+++ Day19_BinarySearch/Binary Search.cpp
@@ -31,3 +31,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+  public:
+    bool binarySearch(vector<int>& arr, int k) {
+        // code here
+        
+        int n=arr.size();
+        
+        int start=0;
+        int end=n-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(arr[mid]==k){
+                return true;
+            }
+            else if(arr[mid]<k){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
