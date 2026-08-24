f170cb14f87b5e8d8b32276fb22201358db3ada7
subodhnarayan <subodhnarayansah@gmail.com>
2026-08-25 00:41:11 +0530

    optimized solution of number of occurance

--- Day23_Binary Search/Number of Occurrence.cpp
+++ Day23_Binary Search/Number of Occurrence.cpp
@@ -33,17 +33,74 @@ using namespace std;
 
 class Solution {
   public:
+    int findFirst(vector<int>&arr, int target){
+        int n=arr.size();
+        int index=-1;
+        
+        int start=0;
+        int end=n-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(arr[mid]==target){
+                index=mid;
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return index;
+        
+    }
+    int findLast(vector<int>&arr, int target){
+        int n=arr.size();
+        int index=-1;
+        
+        int start=0;
+        int end=n-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(arr[mid]==target){
+                index=mid;
+                start=mid+1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return index;
+        
+    }
     int countFreq(vector<int>& arr, int target) {
         // code here
         int n=arr.size();
         int count=0;
         //Brute Force Approach
         //TC=0(n) and SC=(1)
-        for(int i=0;i<n;i++){
-            if(arr[i]==target){
-                count++;
-            }
-        }
-        return count;
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==target){
+        //         count++;
+        //     }
+        // }
+        // return count;
+        
+        //Optimized Approach using Binary Search
+        
+        //TC=0(logn) and SC=0(1)
+        
+        int first=findFirst(arr,target);
+        if(first==-1) return 0;
+        
+        return ((findLast(arr,target)-first)+1);
     }
 };
